f0817c8ece9fea82b311cbf65256436d089386c6
Lenard James <lsmith18@uw.edu>
2019-11-12 10:59:17 -0800

    Weather Icons added.

---
+++ app/src/main/java/edu/uw/tcss450/team3chatapp/ui/WeatherFragment.java
@@ -0,0 +1,32 @@
+package edu.uw.tcss450.team3chatapp.ui;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.uw.tcss450.team3chatapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class WeatherFragment extends Fragment {
+
+
+    public WeatherFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_weather, container, false);
+    }
+
+}
