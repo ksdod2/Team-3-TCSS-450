4176b96143affaa42e92698db3ed0c86c97c24a6
Alex Bledsoe <abledso3@uw.edu>
2019-11-16 00:30:01 -0800

    Add popup menu w/ click listeners to theme selection in settings menu.

--- app/src/main/java/edu/uw/tcss450/team3chatapp/SettingsFragment.java
+++ app/src/main/java/edu/uw/tcss450/team3chatapp/SettingsFragment.java
@@ -7,21 +7,29 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends Fragment implements PopupMenu.OnMenuItemClickListener {
 
 
     public SettingsFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -33,6 +41,32 @@ public class SettingsFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        FrameLayout themeOption = getView().findViewById(R.id.frame_settings_theme);
+        themeOption.setOnClickListener(this::showThemeChangePopup);
+    }
 
+    private void showThemeChangePopup(final View theView) {
+        PopupMenu popup = new PopupMenu(getContext(), theView, Gravity.END);
+        popup.setOnMenuItemClickListener(this);
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.settings_theme, popup.getMenu());
+        popup.show();
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.theme1:
+                Log.d("SETTINGS_MENU", "THEME 1 PRESSED");
+                return true;
+            case R.id.theme2:
+                Log.d("SETTINGS_MENU", "THEME 2 PRESSED");
+                return true;
+            case R.id.theme3:
+                Log.d("SETTINGS_MENU", "THEME 3 PRESSED");
+                return true;
+            default:
+                return false;
+        }
     }
 }
